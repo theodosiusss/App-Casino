600def6053fb982940c1a6f04c8d049fad4c2729
Theodosiusss <1202@htl.rennweg.at>
2025-09-23 11:09:35 +0200

    skibidi acoount hell yeah fix fix fix

--- app/components/main/header.ts
+++ app/components/main/header.ts
@@ -1,6 +1,5 @@
 import Component from '@glimmer/component';
 import {service} from '@ember/service';
-import {action} from "@ember/object";
 import type UserService from "app-casino/services/user";
 
 export default class HeaderComponent extends Component {
--- app/components/user/payment.ts
+++ app/components/user/payment.ts
@@ -1,11 +1,9 @@
 import Component from '@glimmer/component';
-import { action } from '@ember/object';
 import { service } from '@ember/service';
-import {tracked} from "@glimmer/tracking";
 import type UserService from "app-casino/services/user";
 
 export default class PaymentComponent extends Component {
-  @service   @service declare user: UserService;
+  @service declare user: UserService;
 
 
 }
