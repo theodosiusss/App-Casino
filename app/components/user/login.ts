8ae4eb61fe72e0c8df47f22018c40584dccfadb2
Theodosiusss <1202@htl.rennweg.at>
2025-09-23 12:50:55 +0200

    yoyoyo fix fix

--- app/components/user/login.ts
+++ app/components/user/login.ts
@@ -3,7 +3,7 @@ import { action } from '@ember/object';
 import { service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 import type UserService from 'app-casino/services/user';
-import type Router from "app-casino/router";
+import type Router from 'app-casino/router';
 
 export default class LoginComponent extends Component {
   @service declare user: UserService;
@@ -20,7 +20,6 @@ export default class LoginComponent extends Component {
   submitLogin(event: Event) {
     event.preventDefault();
     this.user.setName(this.username);
-    setTimeout(()=>this.router.transitionTo('/'), 1000)
-
+    setTimeout(() => this.router.transitionTo('/'), 1000);
   }
 }
